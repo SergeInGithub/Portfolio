fa56a84076d74a865e693456c3408fbc2b3842f2
SergeInGithub <nyiriserge@gmail.com>
2023-06-27 19:08:56 +0200

    ft(projectsCard): add dark mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ProjectsCard from '../components/ProjectsCard';
+import { ThemeToggle } from '../components/ThemeToggle';
 
 export function Home() {
-  return <ProjectsCard />;
+  return (
+    <div>
+      <ThemeToggle />
+      <ProjectsCard />;
+    </div>
+  );
 }
